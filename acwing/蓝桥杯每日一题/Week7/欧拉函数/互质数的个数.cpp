5ca02bd73db1f7e4c1ed633e67611554799a4125
弘树 <qilei.zhou6@gmail.com>
2024-06-06 08:38:12 +0800

    2024-06-06打卡

---
+++ acwing/蓝桥杯每日一题/Week7/欧拉函数/互质数的个数.cpp
@@ -0,0 +1,55 @@
+/**
+ * @file 互质数的个数.cpp
+ * @author horiki
+ * @version 0.1
+ * @date 2024-06-06
+ * @copyright Copyright (c) 2024
+ * 
+ * @brief 
+ * 
+ */
+
+#include <iostream>
+using namespace std;
+
+typedef long long LL;
+
+const int P = 998244353;
+
+LL qmi(LL a, LL k)
+{
+	LL res = 1;
+	while (k)
+	{
+		if (k & 1) res = res * a;
+		k >>= 1;
+		a *= a;
+	}
+	return res;
+}
+
+int get_oular(LL x)
+{
+	LL res = x;
+	for (LL i = 2; i <= x / i; i ++)
+		if (x % i == 0)
+		{
+			res = res / i * (i - 1) % P;
+			while (x % i == 0) x /= i;
+		}
+	if (x > 1) res = res / x * (x - 1) % P;
+	return res;
+}
+
+int main()
+{
+	LL a, b; cin >> a >> b;
+	if (a == 1)
+	{
+		cout << 0;
+		return 0;
+	}
+
+	cout << (LL)get_oular(a) * qmi(a, b - 1) % P;
+	return 0;
+}
