eb03c4d31f4688a8415ec4a9415e59634fe24c8a
弘树 <qilei.zhou6@gmail.com>
2024-06-11 19:34:53 +0800

    2024-06-11打卡

--- acwing/蓝桥杯每日一题/Week3/回溯/糖果.cpp
+++ acwing/蓝桥杯每日一题/Week3/回溯/糖果.cpp
@@ -2,25 +2,54 @@
  * @file 糖果.cpp
  * @author horiki
  * @version 0.1
- * @date 2024-06-06
+ * @date 2024-06-11
  * @copyright Copyright (c) 2024
  * 
  * @brief 
- * 
+ * 	一共有m种糖果，但是问题是让求最少需要购买多少包糖果
+ * 	在DFS的过程中如何保证这个结果是最少的？
+ * 	使用二分答案 + DFS ？
+ * 	-----------------------------------
+ * 	状态压缩DP：用一个数的二进制来表示一个合法的状态
  */
 
 #include <iostream>
-#include <vector>
-#include <utility>
-#include <algorithm>
+#include <cstring>
 using namespace std;
 
-const int N = ;
+const int N = 110, M = 20;
+int n, m, k;
+int w[N][M], dp[1 << M];
 
 int main()
 {
-	int n, m, k; cin >> n >> m >> k;
-	
+	cin >> n >> m >> k;
+	memset(dp, -1, sizeof dp);
 
+	for (int i = 1; i <= n; i ++)
+		for (int j = 1; j <= k; j ++)
+		{
+			cin >> w[i][j];
+		    w[i][j] --;
+		}
+
+	int tar = 1 << m;
+	dp[0] = 0;
+	for (int i = 1; i <= n; i ++)
+	{
+		int t = 0;
+		for (int j = 1; j <= k; j ++) 
+			t |= (1 << w[i][j]);
+		// 得到这包糖果的状态表示t
+		for (int u = 0; u < tar; u ++)
+		{
+			if (dp[u] == -1) continue;
+			int v = u | t;
+			if (dp[v] == -1 || dp[v] > dp[u] + 1)
+				dp[v] = dp[u] + 1;
+		}
+	}
+
+	cout << dp[tar - 1];
 	return 0;
 }
--- acwing/蓝桥杯每日一题/Week4/Flood Fill/扫雷.cpp
+++ acwing/蓝桥杯每日一题/Week4/Flood Fill/扫雷.cpp
@@ -1,8 +1,8 @@
 /**
  * @file 扫雷.cpp
  * @author horiki
- * @version 0.1
- * @date 2024-05-28
+ * @version 0.2
+ * @date 2024-05-28 2024-06-11
  * @copyright Copyright (c) 2024
  * 
  * @brief 
@@ -10,104 +10,71 @@
  */
 
 #include <iostream>
-#include <queue>
 #include <cstring>
-#include <utility>
-
-#define x first
-#define y second
 using namespace std;
-
-typedef pair<int, int> PII;
 
 const int N = 310;
 int n;
 char g[N][N];
-bool st[N][N];
-
-queue<PII> q;
+int st[N][N];
 
 const int dx[] = {1, -1, 0, 0, -1, -1, 1, 1};
 const int dy[] = {0, 0, 1, -1, -1, 1, -1, 1};
 
-bool check(int x, int y)
-{
-	for (int i = 0; i < 8; i ++)
-	{
-		int a = x + dx[i], b = y + dy[i];
-		if (a < 1 || a > n || b < 1 || b > n) continue;
-		if (g[a][b] != '.') return false;
-	}
-	return true;
-}
-
 void dfs(int x, int y)
 {
-	st[x][y] = true;
+	int t = st[x][y];
+	st[x][y] = -1;
+	if (t) return ;
 	for (int i = 0; i < 8; i ++)
 	{
-		int a = x + dx[i], b = y + dy[i];
-		if (a < 1 || a > n || b < 1 || b > n) continue;
-		if (!st[a][b] && check(a, b)) dfs(a, b);
-		st[a][b] = true;
+		int u = x + dx[i], v = y + dy[i];
+		if (u < 1 || u > n || v < 1 || v > n || st[u][v] == -1) continue;
+		dfs(u, v);
 	}
-}
-
-int flood_fill()
-{
-	int res = 0;
-	while (q.size())
-	{
-		auto p = q.front(); q.pop();
-		if (st[p.x][p.y]) continue;
-		res ++;
-		st[p.x][p.y] = true;
-		for (int i = 0; i < 8; i ++)
-		{
-			int a = p.x + dx[i], b = p.y + dy[i];
-			if (a < 1 || a > n || b < 1 || b > n || st[a][b]) continue;
-			st[a][b] = true;
-		}
-	}
-	return res;
-}
-
-int rest_fill()
-{
-	int res = 0;
-	for (int i = 1; i <= n; i ++)
-		for (int j = 1; j <= n; j ++)
-			if (!st[i][j] && g[i][j] == '.')
-				res ++;
-	return res;
 }
 
 int main()
 {
-	int t; cin >> t;
-	for (int k = 1; k <= t; k ++)
+	int k; cin >> k;
+	for (int t = 1; t <= k; t ++)
 	{
-		memset(st, 0, sizeof st);
 		cin >> n;
 		for (int i = 1; i <= n; i ++)
 			for (int j = 1; j <= n; j ++)
 				cin >> g[i][j];
 
-		// for (int i = 1; i <= n; i ++)
-		// 	for (int j = 1; j <= n; j ++)
-		// 		if (check(i, j)) q.push({i, j});
+		for (int i = 1; i <= n; i ++)
+			for (int j = 1; j <= n; j ++)
+			{
+				if (g[i][j] == '*') st[i][j] = -1;
+				else 
+				{
+					st[i][j] = 0;
+					for (int u = 0; u < 8; u ++)
+					{
+						int x = i + dx[u], y = j + dy[u];
+						if (x < 1 || x > n || y < 1 || y > n) continue;
+						if (g[x][y] == '*') st[i][j] ++;
+					}
+				}
+			}
+
 		int ans = 0;
 		for (int i = 1; i <= n; i ++)
 			for (int j = 1; j <= n; j ++)
-				if (!st[i][j] && check(i, j)) 
+				if (!st[i][j]) 
 				{
 					dfs(i, j);
 					ans ++;
 				}
-		
-		ans += rest_fill();
-		printf("Case #%d: %d\n", k, ans);
-	}
 
+		for (int i = 1; i <= n; i ++)
+			for (int j = 1; j <= n; j ++)
+				if (st[i][j] != -1) ans ++;
+
+		printf("Case #%d: %d\n", t, ans);
+
+	}
 	return 0;
 }
