b1ba8dfb9ec5b86280d50700b4c9f3dbb7717cbd
弘树 <qilei.zhou6@gmail.com>
2024-06-12 15:19:03 +0800

    2024-06-12打卡

---
+++ acwing/蓝桥杯每日一题/Week3/回溯/生日蛋糕.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file 生日蛋糕.cpp
+ * @author horiki
+ * @version 0.1
+ * @date 2024-06-12
+ * @copyright Copyright (c) 2024
+ * 
+ * @brief 
+ * 	1.从下向上搜索
+ * 	2.半径和高度都从大到小枚举
+ * 	3.如果min(v(depth)) + v > n 或者 min(S(depth)) + s > ans 直接 return
+ * 	4.
+ */
+
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <utility>
+#include <algorithm>
+using namespace std;
+
+typedef long long LL;
+
+const int N = ;
+
+int main()
+{
+	int n, m; cin >> n >> m;
+
+
+	return 0;
+}
