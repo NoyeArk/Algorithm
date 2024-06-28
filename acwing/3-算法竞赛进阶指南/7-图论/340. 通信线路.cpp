8eec0a17d6562a5433b4c895ea4d66972556473e
NoyeArk <qilei.zhou6@gmail.com>
2024-06-28 10:07:53 +0800

    2024-06-28打卡

---
+++ acwing/3-算法竞赛进阶指南/7-图论/340. 通信线路.cpp
@@ -0,0 +1,28 @@
+/**
+ * @file 340. 通信线路.cpp
+ * @author horiki
+ * @version 0.1
+ * @date 2024-06-28
+ * @copyright Copyright (c) 2024
+ * 
+ * @brief 
+ * 
+ */
+
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <utility>
+#include <algorithm>
+using namespace std;
+
+typedef long long LL;
+
+const int N = ;
+
+int main()
+{
+	
+
+	return 0;
+}
