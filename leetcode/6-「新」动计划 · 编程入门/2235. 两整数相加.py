04d6cc62fa9323ebbe5d696fdb86b43254c39d20
NoyeArk <qilei.zhou6@gmail.com>
2024-06-29 15:27:10 +0800

    2024-06-29打卡

---
+++ leetcode/6-「新」动计划 · 编程入门/2235. 两整数相加.py
@@ -0,0 +1,14 @@
+'''
+ @file 2235. 两整数相加.py
+ @author horiki
+ @version 0.1
+ @date 2024-06-29
+ @copyright Copyright (c) 2024
+ 
+ @brief 
+ 
+'''
+ 
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        return num1 + num2
