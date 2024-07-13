e0cf42e43bef2023776ca9980da0dc5b9375ba3b
NoyeArk <qilei.zhou6@gmail.com>
2024-07-13 22:52:36 +0800

    2024-07-13打卡

---
+++ acwing/3-算法竞赛进阶指南/2-基本数据结构/2-队列/133. 蚯蚓.cpp
@@ -0,0 +1,28 @@
+/**
+ * @file 133. 蚯蚓.cpp
+ * @author horiki
+ * @version 0.1
+ * @date 2024-07-13
+ * @copyright Copyright (c) 2024
+ * 
+ * @brief 
+ * 
+ */
+
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <utility>
+#include <algorithm>
+using namespace std;
+
+typedef long long LL;
+
+const int N = ;
+
+int main()
+{
+	
+
+	return 0;
+}
---
+++ leetcode/6-「新」动计划 · 编程入门/709. 转换成小写字母.py
@@ -0,0 +1,14 @@
+'''
+ @file 709. 转换成小写字母.py
+ @author horiki
+ @version 0.1
+ @date 2024-07-13
+ @copyright Copyright (c) 2024
+ 
+ @brief 
+ 
+'''
+
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        return s.lower()
